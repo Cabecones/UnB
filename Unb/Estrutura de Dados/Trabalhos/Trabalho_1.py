a317965839adfd5f4cc4bfcfbbd487841c7928e3
Keven Luan <kevenluan.sb@hotmail.com>
2021-04-04 22:57:15 -0300

    Trabalho

--- Unb/Estrutura de Dados/Trabalhos/Trabalho_1.py
+++ Unb/Estrutura de Dados/Trabalhos/Trabalho_1.py
@@ -49,13 +49,15 @@ def decifrar(deque, texto_cifrado, chave):
 
 
 def selecionar_subconjunto_missoes():
-    horas, mbinario, columindice, alfabeto, quantidade_posicoes, missoes_cifradas = int(input()), int(input()), int(input()), int(input()), int(input()), int(input())
+    horas, mbinario, columindice, alfabeto, quantidade_posicoes, missoes_cifradas = int(input()), int(input()), int(input()), input(), int(input()), int(input())
 
+    d = Deque()
     missoes = []
+    adicionar_alfabeto(d, alfabeto)
 
     for i in range(missoes_cifradas):
         missao = input()
-        missao_limpa = i[1:len(i) - 1]
+        missao_limpa = missao[1:len(missao) - 1]
         missao = decifrar(d, missao_limpa, quantidade_posicoes)
         missoes.append(missao.split(','))
 
@@ -69,11 +71,37 @@ def selecionar_subconjunto_missoes():
             else:
                 erva_santa_maria[i].append(-1)
 
-    def mochila():
-        if M[missoes_cifradas][horas] == -1:
-            if W[missoes_cifradas] > horas:
-                M[missoes_cifradas][horas] = mochila(missoes_cifradas - 1, v, w, horas)
+    desconhecido, atividades = [atividade[2] for atividade in missoes], [atividade[1] for atividade in missoes]
+
+    def mochila(missoes_cifradas, desconhecido, atividades, horas):
+        if erva_santa_maria[missoes_cifradas][horas] == - 1:
+            if int(atividades[missoes_cifradas - 1]) > horas:
+                erva_santa_maria[missoes_cifradas][horas] = mochila(missoes_cifradas - 1, desconhecido, atividades, horas)
             else:
-                use = v[missoes_cifradas] + mochila(missoes_cifradas - 1, v, w, horas - w[missoes_cifradas])
-                naouse = mochila(missoes_cifradas - 1, v, w, horas)
-                M[missoes_cifradas][horas] = max{use, naouse}
+                use = int(desconhecido[missoes_cifradas - 1]) + mochila(missoes_cifradas - 1, desconhecido, atividades, horas - int(atividades[missoes_cifradas - 1]))
+                naouse = mochila(missoes_cifradas - 1, desconhecido, atividades, horas)
+                erva_santa_maria[missoes_cifradas][horas] = max(use, naouse)
+        return erva_santa_maria[missoes_cifradas][horas]
+
+    def solucao(erva, missoes, horas):
+        solucao = []
+        for i in range(len(erva) - 1, 0, -1):
+            if erva[i][horas] == erva[i - 1][horas - int(missoes[i - 1][1])] + int(missoes[i - 1][2]):
+                solucao.append(missoes[i - 1])
+                horas -= int(solucao[-1][1])
+        return solucao
+
+    def impressora(S, horas, columindice, mbinario):
+        tempo_restante= horas - sum([int(missao[1]) for missao in S])
+        valor = sum([int(missao[2]) for missao in S])
+        S = [[str(num) for num in missao] for missao in S]
+        algum_num = [algum for algum in range(4) if algum != columindice]
+        S = sorted(S, key=lambda x: (x[columindice], x[algum_num[0]], x[algum_num[1]], x[algum_num[2]]))
+        if mbinario: [print(', '.join(missao)) for missao in S]
+        print(f'Tempo restante: {tempo_restante}')
+        print(f'Valor: {valor}')
+
+    mochila(missoes_cifradas, desconhecido, atividades, horas)
+    solussao = solucao(erva_santa_maria, missoes, horas)
+    impressora(solussao, horas, columindice, mbinario)
+
