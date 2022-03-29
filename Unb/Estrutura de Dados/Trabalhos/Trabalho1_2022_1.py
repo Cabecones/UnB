6cabad2f288c671b37797585dc9fd5d3c35b6ff2
Keven <kevenluan.sb@hotmail.com>
2022-03-29 00:20:45 -0300

    Novas

--- Unb/Estrutura de Dados/Trabalhos/Trabalho1_2022_1.py
+++ Unb/Estrutura de Dados/Trabalhos/Trabalho1_2022_1.py
@@ -1,4 +1,5 @@
 import itertools
+
 class Deque:
     def __init__(self):
         self.__items = []
@@ -27,7 +28,9 @@ class Deque:
             sdeque += i
         return sdeque
 
+
 deque = Deque()
+
 '''''
 while True:
     deque1 = Deque()
@@ -46,9 +49,10 @@ while True:
     deque.add_rear(deque1)
 '''''
 
+
 def deyodafy():
     frase = list(input().split())
-    frase.remove('deYodafy') #retirar o yodafy na função de cima, pois ele será um comando
+    frase.remove('deYodafy')  # retirar o yodafy na função de cima, pois ele será um comando
 
     for palavra in frase:
         points = '!@#$.'
@@ -56,45 +60,91 @@ def deyodafy():
             if char in points:
                 frase.append(palavra.replace(char, ''))
                 frase.remove(palavra)
-                frase.insert(0, frase[0].replace(frase[0], frase[0]+char))
+                frase.insert(0, frase[0].replace(frase[0], frase[0] + char))
                 frase.remove(frase[1])
                 frase = frase[::-1]
 
     for palavra in frase:
         print(palavra, end=' ')
 
-#cordenada = input().split()
-#print(cordenada)
+def permutacao():
+    inp_list = [1, 2, 3, 4, 5]
+    permutations = list(itertools.permutations(inp_list))
 
-inp_list = [1, 2, 3, 4]
-permutations = list(itertools.permutations(inp_list))
+    crypt = ['-', '+', '+', '-']
 
-crypt = ['-', '-', '-']
+    for combi in permutations:
+        condition = 0
+        num = 0
+        for sin in crypt:
+            num_ant = combi[num]
+            if sin == '-':
+                if num_ant > combi[num + 1]:
+                    condition = 1
+                    num += 1
+                else:
+                    condition = 0
+                    break
+            elif sin == '+':
+                if num_ant < combi[num + 1]:
+                    condition = 1
+                    num += 1
+                else:
+                    condition = 0
+                    break
+        if condition == 1:
+            for i in combi:
+                print(i, end='')
+            break
 
-for combi in permutations:
-    condition = 0
-    num = 0
-    for sin in crypt:
-        num_ant = combi[num]
-        if sin == '-':
-            if num_ant > combi[num + 1]:
-                condition = 1
-                num += 1
-            else:
-                condition = 0
-                break
-        elif sin == '+':
-            if num_ant < combi[num + 1]:
-                condition = 1
-                num += 1
-            else:
-                condition = 0
-                break
-    if condition == 1:
-        for i in combi:
-            print(i, end='')
-        break
+def cordenadas():
+    cordenada = input().split(' ', 1)
+    comando = []
+    comando.append(cordenada[0])
+    cordenada.remove('merge')
+    cordenada = cordenada[0].split('] ')
+    cordenada = sorted(cordenada)
+    cordenadas = []
+
+    for palavra in cordenada:
+        points = '['
+        for char in palavra:
+            if char in points:
+                cordenadas.append(palavra.replace(char, ''))
+
+    for palavra in cordenadas:
+        points = ']'
+        for char in palavra:
+            if char in points:
+                cordenadas.append(palavra.replace(char, ''))
+                cordenadas.remove(palavra)
+
+    for num in range(3):
+        for palavra in cordenadas:
+            points = ','
+            for char in palavra:
+                if char in points:
+                    cordenadas.append(palavra.replace(char, ''))
+                    cordenadas.remove(palavra)
 
 
 
+    cordenadas = sorted(cordenadas)
+    x = []
+    y = []
 
+    for l in cordenadas:
+        a, b = l.split()
+        x.append(a)
+        y.append(b)
+
+    print(x)
+    print(y)
+
+    i = 0
+    j = 1
+
+    for cord in range(len(cordenadas)//2):
+        print(f'[{x[i]}, {y[j]}]', end=' ')
+        i += 2
+        j += 2
