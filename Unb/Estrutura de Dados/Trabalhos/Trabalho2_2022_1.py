04302f7c8913f9ac82e7b191e502fceaff9da121
Cabecones <kevenluan.sb@hotmail.com>
2022-04-28 19:11:02 -0300

    Trabalho final, acho

--- Unb/Estrutura de Dados/Trabalhos/Trabalho2_2022_1.py
+++ Unb/Estrutura de Dados/Trabalhos/Trabalho2_2022_1.py
@@ -6,13 +6,18 @@ class No:
         self.children = []
         self.parent = parent
 
+    def show(self, level=0):
+        print(f'{"---" * level}{self.item}')
+        for child in sorted(self.children, key=lambda x: x.item):
+            child.show(level + 1)
+
     def mkdir(self, path):
-        if path.length == 0:
+        if len(path) == 0:
             return
         nome = path.pop(0)
         for child in self.children:
             if child.item == nome:
-                if path.length == 0:
+                if len(path) == 0:
                     return print('DIRETÓRIO JÁ EXISTE')
                 return child.mkdir(path)
         novo = No(nome, self)
@@ -20,64 +25,108 @@ class No:
         novo.mkdir(path)
 
     def touch(self, path):
-        if path.length == 0:
+        if len(path) == 0:
             return
-        if path.length == 1:
+        if len(path) == 1:
             nome1 = path.pop(0)
+            print(nome1)
             for child in self.children:
                 if child.item == nome1:
                     print('ARQUIVO JÁ EXISTE')
-                    break
+                    return
+            return self.children.append(No(nome1, self))
         nome = path.pop(0)
         for child in self.children:
             if child.item != nome:
                 print('CAMINHO INVÁLIDO')
-                break
+                return
 
     def rm(self, path):
-        if path.length == 0:
-            return
-        path = list(reversed(path))
-        nome = path.pop(0)
-        i = 1
+        print(path)
+        if len(path) == 1:
+            for child in self.children:
+                if child.item == path[0]:
+                    child.item = None
+                return
+            return print('ARQUIVO ou DIRETÓRIO não existe')
         for child in self.children:
-            if child.item == nome:
-                child.item = None
-                i = 0
-        if i == 1:
+            if child.item == path[0]:
+                path.pop(0)
+                return self.rm(path)
+        print('CAMINHO INVÁLIDO')
+
+    def find(self, name, prefix='/'):
+        self.children.sort(key=lambda x: x.item)
+        lst = []
+        for child in self.children:
+            if child.item == name:
+                lst.append(prefix + child.item)
+            lst = lst + child.find(name, prefix + child.item + '/')
+        return lst
+
+    def mv(self, origem, destino, curent):
+        if curent != '/':
+            if len(origem) == 1:
+                for child in self.children:
+                    if child.item == origem[0]:
+                        if child.item != destino[0]:
+                            mover = child.item.pop()
+                            No.mkdir(mover)
+                        return
+                return print('ARQUIVO ou DIRETÓRIO não existe')
             print('CAMINHO INVÁLIDO')
-        #ADICIONAR ISSO (Se o diretório corrente for removido (pwd), o novo diretório corrente será o "/".)
+        else:
+            return
 
-    def find(self, path):
-        if self.root == None:
-            return None  # se arvore vazia
-        atual = self.root  # começa a procurar desde o pwd
-        while atual.item != path:  # enquanto nao encontrou
-            if atual == None:
-                return None  # encontrou uma folha -> sai
-        return atual  # terminou o laço while e chegou aqui é pq encontrou item
+    def cd(self, path):
+        if len(path) == 0:
+            return self
+        atual = path.pop(0)
+        if atual == '.':
+            return self.cd(path)
+        if atual == '..':
+            return self.parent.cd(path)
+        for child in self.children:
+            if child.item == atual:
+                return child.cd(path)
+        print('CAMINHO INVÁLIDO')
 
-def cd(caminho):
-    curent = caminho
-    return
+    def pwd(self):
+        return print(self.item)
 
-def ponto(curent):
-    return curent
+curent = No('/', None)
 
 while True:
-    curent = '/'
     entrada = input()
+    comando = ''
+    caminho = []
+    origem = []
+    destino = []
     if entrada == 'end':
         break
     if entrada != 'show' and entrada != 'pwd':
         if 'mv' in entrada:
             comando, origem, destino = entrada.split()
-            print(comando, origem, destino)
-        elif 'cd' in entrada:
-            comando, caminho = entrada.split()
-            print(caminho)
         else:
             comando, caminho = entrada.split()
             caminho = caminho.split('/')
             caminho = list(filter(None, caminho))
-            print(caminho)
+    else:
+        comando = entrada
+
+    if comando == 'cd':
+        curent.cd(caminho)
+    if comando == 'pwd':
+        curent.pwd()
+    if comando == 'mkdir':
+        curent.mkdir(caminho)
+    if comando == 'mv':
+        curent.mv(origem, destino, curent)
+    if comando == 'touch':
+        curent.touch(caminho)
+    if comando == 'rm':
+        curent.rm(caminho)
+    if comando == 'find':
+        print(curent.find(caminho))
+    if comando == 'show':
+        curent.show()
